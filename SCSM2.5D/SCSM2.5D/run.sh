946e8c7053ccbd17ffba9ca0556d710757d46a2d
mswonscsm <167202261+mswonscsm@users.noreply.github.com>
2024-04-17 14:35:03 +0400

    Delete SCSM2.5D directory

--- SCSM2.5D/SCSM2.5D/run.sh
+++
@@ -1,6 +0,0 @@
-gfortran -mcmodel=medium  -c C_DF.f90 Grid_Model.f90 Interp.f90 MATRIX_YYXZ.f90 MS_DF.f90 Gauss_Quad.f90 Viscoelastic.f90 Viscoelastic2.f90 
-gfortran -mcmodel=medium  C_DF.o Grid_Model.o Interp.o MATRIX_YYXZ.o MS_DF.o Gauss_Quad.o Viscoelastic.o Viscoelastic2.o MainOMP.f90 -fopenmp 
-export omp_set_num_threads=2 
-
-./a.out
-
